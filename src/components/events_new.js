75cdf04ac7c8ed28cbddf00468266868039ebc16
TakaHasui <emeth.2501@gmail.com>
2021-03-28 22:17:35 +0900

    Postイベントの作成 - redux-form

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm} from 'redux-form'
 import { Link } from 'react-router-dom'
 
-// import { postEvents } from '../actions';
+import { postEvent } from '../actions';
 
 class EventsNew extends Component {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  renderField(field) {
+    const { input, label, type, meta: { touched, error }} = field
+
+    return (
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    )
+  }
+
+async onSubmit(values) {
+  await this.props.postEvent(values)
+  this.props.history.push('/')
+}
+
   render() {
+    const { handleSubmit } = this.props
     return (
       <React.Fragment>
         <div>新規作成画面</div>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <div>
+            <Field lavel="Title" name="title" type="text" component={this.renderField}/>
+            <Field lavel="Body" name="body" type="text" component={this.renderField}/>
+
+            <div>
+             <input type="submit" value="Submit" disabled={false} />
+             <Link to="/">Cancel</Link>
+            </div>
+          </div>
+        </form>
       </React.Fragment>
     )
   }
 }
 
-// const mapDispatchToProps = dispatch => ({ postEvent })
+const validate = values => {
+  const errors = {}
+
+  if (!values.title) errors.title = "Enter a title, please."
+  if (!values.body) errors.body = "Enter a body, please."
+
+  return errors
+}
+
+const mapDispatchToProps = ({ postEvent })
 
 // connect: stateとactionをcomponentに関連付ける
-export default connect(null, null)(EventsNew)
+export default connect(null, mapDispatchToProps)(
+  // Error: Field must be inside a component decorated with reduxForm()
+  reduxForm({ validate, form: 'eventNewForm'})(EventsNew)
+)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import count from './count'
 import events from './events'
 
 // 複数のReducerを使う場合は、カンマ区切りで{}内に複数定義
-export default combineReducers({ count, events })
+export default combineReducers({ count, events, form })
